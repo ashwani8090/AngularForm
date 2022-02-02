3dd063c318013ab431462f49149839c1022ae3f0
ashwani8090 <ashwani8090singh@gmail.com>
2022-02-02 21:28:04 +0530

    1 file modified

--- src/app/generic-form/generic-form.module.ts
+++ src/app/generic-form/generic-form.module.ts
@@ -4,22 +4,24 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CustomFormComponent } from './custom-form/custom-form.component';
 import { GenericInputRowComponent } from './custom-form/generic-input-row/generic-input-row.component';
 import { MatSelectModule } from '@angular/material/select';
-import { FormServiceService } from './custom-form/service/form-service.service'
-import { MatSelectSearchComponent } from './custom-form/mat-select-search/mat-select-search.component'
+import { MatIconModule } from '@angular/material/icon';
+import { FormServiceService } from './custom-form/service/form-service.service';
+import { MatSelectSearchComponent } from './custom-form/mat-select-search/mat-select-search.component';
 
 @NgModule({
   declarations: [
     CustomFormComponent,
     GenericInputRowComponent,
-    MatSelectSearchComponent
+    MatSelectSearchComponent,
   ],
   imports: [
     CommonModule,
     MatSelectModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatIconModule,
   ],
   exports: [CustomFormComponent],
-  providers: [FormServiceService]
+  providers: [FormServiceService],
 })
-export class GenericFormModule { }
+export class GenericFormModule {}
